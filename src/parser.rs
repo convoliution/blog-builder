93e1588929034d91dde117f72a0292d5ae6e0f95
Michael Liu <miliu@protonmail.com>
2018-11-08 12:46:59 -0800

    initialize convert submodule

--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,14 @@
+use failure::Error;
+
+use std::io::{BufRead, Lines};
+
 #[derive(Debug, Fail)]
 pub enum ParseError {
     #[fail(display = "input string was empty")]
     EmptyText,
+}
+mod convert {
+    use std::str::Chars;
+
+    use parser::ParseError;
 }
