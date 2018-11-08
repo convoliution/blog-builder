2a4505311e2dabee1305e255fc7bda6335422c7e
Michael Liu <miliu@protonmail.com>
2018-11-08 12:49:49 -0800

    create fn to parse headings

--- src/parser.rs
+++ src/parser.rs
@@ -7,8 +7,34 @@ pub enum ParseError {
     #[fail(display = "input string was empty")]
     EmptyText,
 }
+
 mod convert {
     use std::str::Chars;
 
     use parser::ParseError;
+
+    pub fn heading(buf: String) -> Result<String, String> {
+        let mut level = 0;
+        let mut chars = buf.chars();
+
+        while let Some(c) = chars.next() {
+            match c {
+                '#' => {
+                    level += 1;
+                    if level > 6 {
+                        return Err(buf);
+                    }
+                },
+                ' ' => {
+                    match text(&mut chars) {
+                        Ok(html) => return Ok(format!("<h{level}>{}</h{level}>", html, level=level)),
+                        Err(_) => return Err(buf),
+                    }
+                },
+                 _  => return Err(buf),
+            }
+        }
+
+        Err(buf)
+    }
 }
