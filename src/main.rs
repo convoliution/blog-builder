aac15f6b987897f907d3d6580ff8f8b0b770b1cb
Michael Liu <miliu@protonmail.com>
2018-11-08 12:31:37 -0800

    obtain edit date of post

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,14 @@
 extern crate chrono;
 extern crate clap;
 
+mod parser;
+
 use clap::{Arg, App};
+
+use std::fs::metadata;
+
+use chrono::offset::Local;
+use chrono::DateTime;
 
 fn main() {
     let args = App::new("Blog Builder")
@@ -25,4 +32,17 @@ fn main() {
             .takes_value(false)
             .help("Generates all posts and indices"))
         .get_matches();
+
+    if args.is_present("post") {
+        let md_filename = args.value_of("post").unwrap();
+
+        let datetime: DateTime<Local> = metadata(md_filename)
+            .expect(&format!("failed to access {}", md_filename))
+            .modified().unwrap()
+            .into();
+        let auth_date = format!("{}", datetime.format("%B %e, %Y"));
+
+    } else if args.is_present("all") {
+        //let file_names =
+    }
 }
