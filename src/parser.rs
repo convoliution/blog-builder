24bbb050950f9f73571201bd883c74e96d4b774c
Michael Liu <miliu@protonmail.com>
2018-11-08 13:47:07 -0800

    create Parser struct and constructor

--- src/parser.rs
+++ src/parser.rs
@@ -1,11 +1,23 @@
 use failure::Error;
 
-use std::io::{BufRead, Lines};
-
 #[derive(Debug, Fail)]
 pub enum ParseError {
     #[fail(display = "input string was empty")]
     EmptyText,
+}
+
+pub struct Parser<I: Iterator<Item=Result<String, Error>>> {
+    lines: I,
+    hold: Option<String>,
+}
+
+impl<I> Parser<I> where I: Iterator<Item=Result<String, Error>> {
+    pub fn new(lines: I) -> Parser<I> {
+        Parser {
+            lines,
+            hold: None,
+        }
+    }
 }
 
 mod convert {
