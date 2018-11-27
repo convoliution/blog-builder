828e83c722ea2a4b12242b05ef63b4afabce6d93
Michael Liu <miliu@protonmail.com>
2018-11-27 11:42:13 -0800

    take string slices

--- src/parser/convert.rs
+++ src/parser/convert.rs
@@ -1,15 +1,15 @@
 use std::str::Chars;
 
-pub fn is_unord_list_item(buf: String) -> bool {
-    buf.starts_with("- ")
+pub fn is_unord_list_item(md: &str) -> bool {
+    md.starts_with("- ")
 }
 
-pub fn is_ord_list_item(buf: String) -> bool {
-    buf.starts_with("1. ")
+pub fn is_ord_list_item(md: &str) -> bool {
+    md.starts_with("1. ")
 }
 
-pub fn is_heading(buf: String) -> bool {
-    let mut chars = buf.chars();
+pub fn is_heading(md: &str) -> bool {
+    let mut chars = md.chars();
 
     let mut num_hashes = 0;
     while let Some(c) = chars.next() {
@@ -28,12 +28,12 @@ pub fn is_heading(buf: String) -> bool {
     false
 }
 
-pub fn is_quote(buf: String) -> bool {
-    buf.starts_with("> ")
+pub fn is_quote(md: &str) -> bool {
+    md.starts_with("> ")
 }
 
-pub fn is_image(buf: String) -> bool {
-    let mut chars = buf.chars();
+pub fn is_image(md: &str) -> bool {
+    let mut chars = md.chars();
 
     if let Some('[') = chars.next() {
         let mut chars = chars.skip_while(|c| *c != ']').skip(1);
@@ -52,13 +52,13 @@ pub fn is_image(buf: String) -> bool {
     false
 }
 
-pub fn unord_list(buf: String) -> Result<String, String> {
-    if !buf.lines().all(|line| line.starts_with("- ")) {
-        Err(buf)
+pub fn unord_list(md: &str) -> Result<String, &str> {
+    if !md.lines().all(|line| line.starts_with("- ")) {
+        Err(md)
     } else {
-        let items_html: String = buf.lines()
+        let items_html: String = md.lines()
             .map(|line| line.chars().skip(2))
-            .map(|chars| text(chars.collect::<String>()).trim())
+            .map(|chars| text(chars.collect::<String>().as_str()).trim())
             .map(|item_html| format!("<li>{}</li>", item_html))
             .collect();
 
@@ -66,13 +66,13 @@ pub fn unord_list(buf: String) -> Result<String, String> {
     }
 }
 
-pub fn ord_list(buf: String) -> Result<String, String> {
-    if !buf.lines().all(|line| line.starts_with("1. ")) {
-        Err(buf)
+pub fn ord_list(md: &str) -> Result<String, &str> {
+    if !md.lines().all(|line| line.starts_with("1. ")) {
+        Err(md)
     } else {
-        let items_html: String = buf.lines()
+        let items_html: String = md.lines()
             .map(|line| line.chars().skip_while(|c| c.is_digit(10)).skip(2))
-            .map(|chars| text(chars.collect::<String>()).trim())
+            .map(|chars| text(chars.collect::<String>().as_str()).trim())
             .map(|item_html| format!("<li>{}</li>", item_html))
             .collect();
 
@@ -80,63 +80,63 @@ pub fn ord_list(buf: String) -> Result<String, String> {
     }
 }
 
-pub fn code_block(buf: String) -> Result<String, String> {
-    if !buf.starts_with("```") || !buf.trim_end().ends_with("```") {
-        return Err(buf)
+pub fn code_block(md: &str) -> Result<String, &str> {
+    if !md.starts_with("```") || !md.trim_end().ends_with("```") {
+        return Err(md)
     } else {
-        let (opening_line, code_text) = buf.trim_end()
+        let (opening_line, code_text) = md.trim_end()
             .trim_end_matches('`')
-            .split_at(buf.find('\n').unwrap());
+            .split_at(md.find('\n').unwrap());
 
         let lang = opening_line.trim_start_matches("```").trim();
 
         if lang.is_empty() {
-            Err(buf)
+            Err(md)
         } else {
             Ok(format!("<pre><code class=\"language-{}\">{}</code></pre>", lang, code_text))
         }
     }
 }
 
-pub fn heading(buf: String) -> Result<String, String> {
-    if !buf.starts_with("#") {
-        Err(buf)
+pub fn heading(md: &str) -> Result<String, &str> {
+    if !md.starts_with("#") {
+        Err(md)
     } else {
         let mut level = 0;
-        let mut chars = buf.chars();
+        let mut chars = md.chars();
 
         while let Some(c) = chars.next() {
             match c {
                 '#' => {
                     level += 1;
                     if level > 6 {
-                        return Err(buf);
+                        return Err(md);
                     }
                 },
-                ' ' => return Ok(format!("<h{level}>{}</h{level}>", text(chars.collect::<String>()).trim(), level=level)),
-                 _  => return Err(buf),
+                ' ' => return Ok(format!("<h{level}>{}</h{level}>", text(chars.collect::<String>().as_str()).trim(), level=level)),
+                 _  => return Err(md),
             }
         }
 
-        Err(buf)
+        Err(md)
     }
 }
 
-pub fn quote(buf: String) -> Result<String, String> {
-    if !buf.starts_with("> ") {
-        Err(buf)
+pub fn quote(md: &str) -> Result<String, &str> {
+    if !md.starts_with("> ") {
+        Err(md)
     } else {
-        let mut chars = buf.chars().skip(2);
+        let mut chars = md.chars().skip(2);
 
-        Ok(format!("<blockquote>{}</blockquote>", text(chars.collect::<String>()).trim()))
+        Ok(format!("<blockquote>{}</blockquote>", text(chars.collect::<String>().as_str()).trim()))
     }
 }
 
-pub fn image(buf: String) -> Result<String, String> {
-    if !buf.starts_with("[") {
-        Err(buf)
+pub fn image(md: &str) -> Result<String, &str> {
+    if !md.starts_with("[") {
+        Err(md)
     } else {
-        let mut chars = buf.chars().skip(1);
+        let mut chars = md.chars().skip(1);
 
         let mut alt_text = String::new();
 
@@ -151,37 +151,37 @@ pub fn image(buf: String) -> Result<String, String> {
                                 ')' => if chars.collect::<String>().trim().is_empty() {
                                     return Ok(format!("<img src=\"{}\" alt=\"{}\"/>", src, alt_text))
                                 } else {
-                                    return Err(buf)
+                                    return Err(md)
                                 },
                                  _  => src.push(c),
                             }
                         }
 
-                        return Err(buf)
+                        return Err(md)
                     },
-                    _ => return Err(buf),
+                    _ => return Err(md),
                 },
                  _  => alt_text.push(c),
             };
         }
 
-        Err(buf)
+        Err(md)
     }
 }
 
-pub fn paragraph(buf: String) -> Result<String, String> {
-    let md: String = buf.lines()
+pub fn paragraph(md: &str) -> Result<String, &str> {
+    let md = md.lines()
         .map(|line| line.trim_end_matches('\n'))
         .map(|line| line.trim_end_matches('\r'))
-        .collect();
+        .collect::<String>().as_str();
 
     Ok(format!("<p>{}</p>", text(md).trim()))
 }
 
-pub fn text(buf: String) -> String {
-    let mut html = String::with_capacity(buf.len());
+pub fn text(md: &str) -> String {
+    let mut html = String::with_capacity(md.len());
 
-    let mut chars = buf.replace("<", "&lt;").replace(">", "&gt;").chars();
+    let mut chars = md.replace("<", "&lt;").replace(">", "&gt;").chars();
 
     while let Some(c) = chars.next() {
         match c {
